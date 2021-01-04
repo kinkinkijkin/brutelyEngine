20b38214f72dc2b88c909bd2e870ec06234fc8d1
Ari Vermette <soldiertheinsanitywarrior@gmail.com>
2021-01-03 23:14:29 -0500

    collision and distance estimation time

--- src/game/gamedata.nim
+++ src/game/gamedata.nim
@@ -1,4 +1,4 @@
-import glm
+import glm,with,gamemath,sequtils
 
 type
   Extension* = object of RootObj
@@ -13,11 +13,37 @@ type
   Collision* = object of Extension
     colliding*: bool
     scale*: float
-method coll*(this: var Collision, thisposition: Vec3f, that:Collision, thatposition: Vec3f) =
+    activedist*: float
+method nearest*(this: Collision, pos: Vec3f): Vec3f =
+  #varies between collisions.
+  return vec3f(0)
+method coll*(this: var Collision, thispos: Vec3f, that: var Collision, thatpos: Vec3f) =
   #varies between collisions.
   return
 method run*(this: var Collision) =
   this.colliding = false
+
+type
+  CollisionRect* = object of Collision
+    size*: Vec3f
+method genCorners*(this: CollisionRect): array[8, Vec3f] =
+  with this:
+    result[0] = vec3f(size / 2)
+    result[1] = vec3f(-size / 2)
+    result[2] = vec3f(size.x / 2, size.y / 2, -size.z / 2)
+    result[3] = vec3f(-size.x / 2, -size.y / 2, size.z / 2)
+    result[4] = vec3f(size.x / 2, -size.y / 2, -size.z / 2)
+    result[5] = vec3f(-size.x / 2, size.y / 2, size.z / 2)
+    result[6] = vec3f(-size.x / 2, size.y / 2, -size.z / 2)
+    result[7] = vec3f(size.x / 2, -size.y / 2, size.z / 2)
+method nearest*(this: CollisionRect, pos: Vec3f, thatpos: Vec3f): Vec3f =
+  var corners: array[8, Vec3f] = this.genCorners()
+  var cornerdists: array[8, float]
+  for i, corn in corners:
+    cornerdists[i] = dist3D((corn + pos), thatpos)
+  return corners[cornerdists.minIndex()]
+#method coll*(this: var CollisionRect, thispos: Vec3f, that: var Collision, thatpos: Vec3f) =
+  
 
 type
   Extensible* = object of RootObj
@@ -74,7 +100,3 @@ method killable*(this:Simp): bool =
   return (this.alive and not this.invincible)
 method walking*(this:Simp): bool =
   return (this.grounded and this.moving)
-
-type
-  CollidableRect* = object of Collider
-    extents*: tuple[a, b: Vec3f]
---
+++ src/game/gamemath.nim
@@ -0,0 +1,12 @@
+import glm,math
+
+proc dist3D*(v1, v2: Vec3f): float =
+  var step1: Vec3f = vec3f((v2.x - v1.x), (v2.y - v1.y), (v2.z - v1.z))
+  var step2: Vec3f = vec3f(pow(step1.x, 2), pow(step1.y, 2), pow(step1.z, 2))
+  var step3: float = step2.x + step2.y + step2.z
+  return sqrt(step3)
+proc dist2D*(v1, v2: Vec2f): float =
+  var step1: Vec2f = vec2f((v2.x - v1.x), (v2.y - v1.y))
+  var step2: Vec2f = vec2f(pow(step1.x, 2), pow(step1.y, 2))
+  var step3: float = step2.x + step2.y
+  return sqrt(step3)
